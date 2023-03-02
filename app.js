afd6ae5ace5852ce21766d1b4568eab54944f5a8
yuldashali <yuldashalijalol0113@gmali.com>
2023-03-03 02:18:21 +0500

    clock

---
+++ app.js
@@ -0,0 +1,16 @@
+
+
+let hr = document.querySelector("#hr"),
+  mn = document.querySelector("#mn"),
+  sc = document.querySelector("#sc");
+
+setInterval(() => {
+  let day = new Date();
+  let hh = day.getHours() * 30;
+  let mm = day.getMinutes() * 6;
+  let ss = day.getSeconds() * 6;
+
+  hr.style.transform = `rotateZ(${hh + mm / 12}deg)`;
+  mn.style.transform = `rotateZ(${mm}deg)`;
+  sc.style.transform = `rotateZ(${ss}deg)`;
+});
